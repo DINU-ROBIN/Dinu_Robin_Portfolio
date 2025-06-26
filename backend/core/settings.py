b17394d7c591fbb4653ac5a71845110f506368f9
DINU-ROBIN <dinurobin21@gmail.com>
2025-06-26 19:21:11 +0530

    creating superuser5

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,27 +1,27 @@
 import os
 import socket
 import dj_database_url
-import django
+# import django
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')  # adjust if your settings module is different
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')  # adjust if your settings module is different
 
-try:
-    django.setup()
-except Exception as e:
-    print('Django setup failed:', e)
+# try:
+#     django.setup()
+# except Exception as e:
+#     print('Django setup failed:', e)
 
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
-username = "robin"
-password = "210703"
+# User = get_user_model()
+# username = "robin"
+# password = "210703"
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, password=password)
-    print("Superuser 'robin' created!")
-else:
-    print("Superuser 'robin' already exists.")
+# if not User.objects.filter(username=username).exists():
+#     User.objects.create_superuser(username=username, password=password)
+#     print("Superuser 'robin' created!")
+# else:
+#     print("Superuser 'robin' already exists.")
     
     
     
