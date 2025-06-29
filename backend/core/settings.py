688f13eaab95f97948c3b218eaf8d0dd31fcd5c3
DINU-ROBIN <dinurobin21@gmail.com>
2025-06-29 09:42:55 +0530

    fetch db data 3

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,12 +48,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+3^h@rrts_-eu5)2ip0_y#f)i#fy_ygy8$c!p=o=-r3*)9gu_h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-+3^h@rrts_-eu5)2ip0_y#f)i#fy_ygy8$c!p=o=-r3*)9gu_h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['*']
 
 
