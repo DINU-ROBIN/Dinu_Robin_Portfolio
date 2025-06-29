7a39c0be257efa43108d47a1e8e1bfe0e75580e6
DINU-ROBIN <dinurobin21@gmail.com>
2025-06-29 08:55:04 +0530

    fetch db data 1

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -51,7 +51,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+3^h@rrts_-eu5)2ip0_y#f)i#fy_ygy8$c!p=o=-r3*)9gu_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 # ALLOWED_HOSTS = ['*']
